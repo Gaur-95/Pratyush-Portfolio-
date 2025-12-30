de7d500d4d0b91f58891a1e9a045231d16230009
Gaur-95 <137503048+Gaur-95@users.noreply.github.com>
2025-12-30 17:17:11 +0530

    Portfolio

---
+++ src/main/java/org/example/portfolio/controller/HomeController.java
@@ -0,0 +1,22 @@
+package org.example.portfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"","/", "/home"})
+    public String showHomePage(){
+        return "home";
+
+    }
+
+    // Add this for testing
+    @GetMapping("/health")
+    @ResponseBody
+    public String health() {
+        return "App is running on port: " + System.getenv("PORT");
+    }
+}
